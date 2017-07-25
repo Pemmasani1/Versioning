13fad68756adc39df105f4ca579d2b3bb7c9192a
Pemmasani1 <30432155+Pemmasani1@users.noreply.github.com>
2017-07-25 14:32:00 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,9 @@
 const gulp = require('gulp')
 var bump = require('gulp-bump')
+
+gulp.task('bump', function() {
+     return gulp
+          .src('./config.json')
+          .pipe(bump())
+          .pipe(gulp.dest('./'));
+});
